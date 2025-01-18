87376ceffe0897d8050b7e4ea93c6a7e3eb51386
Sanjay <sanjayvarma44433@gmail.com>
2025-01-18 14:37:53 +0530

    Added balls and their prefabs

---
+++ KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Number Jar/NJ_AimlineController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NJ_AimlineController : MonoBehaviour
+{
+    [SerializeField] Transform fruitDropTransform;
+    [SerializeField] Transform BottomTransform;
+
+    float topPos;
+    float bottomPos;
+    float x;
+
+    [SerializeField] LineRenderer lineRenderer;
+
+    private void Update()
+    {
+        x = fruitDropTransform.position.x;
+
+        topPos = fruitDropTransform.position.y;
+        bottomPos = BottomTransform.position.y;
+
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+
+        lineRenderer.SetPosition(0, new Vector3(x, topPos));
+        lineRenderer.SetPosition(1, new Vector3(x, bottomPos));
+    }
+
+    private void OnValidate()
+    {
+        x = fruitDropTransform.position.x;
+
+        topPos = fruitDropTransform.position.y;
+        bottomPos = BottomTransform.position.y;
+
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+
+        lineRenderer.SetPosition(0, new Vector3(x, topPos));
+        lineRenderer.SetPosition(1, new Vector3(x, bottomPos));
+    }
+
+}
