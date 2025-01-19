2c5959763d65090009ba2a43b5deda2bfdc5adca
Sanjay <sanjayvarma44433@gmail.com>
2025-01-19 23:05:49 +0530

    Number Jar Completed

--- KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Number Jar/NJ_GameManager.cs
+++ KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Number Jar/NJ_GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NJ_GameManager : MonoBehaviour
 {
@@ -14,13 +15,34 @@ public class NJ_GameManager : MonoBehaviour
     public float timeOverLimit = 2f;
 
     public TextMeshProUGUI scoreText;
+
+    [SerializeField] GameObject PauseMenu;
+
+    [SerializeField] GameObject gameOverMenu;
+
+    [SerializeField] public TextMeshProUGUI scoreGO;
+
+    private bool ispaused;
     private void Awake()
     {
         if (instance == null) { 
             instance = this;
         }
         scoreText.text = "0";
-        
+
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+        ispaused = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !ispaused)
+        {   
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            ispaused = true;
+        }
     }
 
     public void IncreaseScore(int amount)
@@ -31,8 +53,30 @@ public class NJ_GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        Time.timeScale = 0f;
+        gameOverMenu.SetActive(true);
+        scoreGO.text = "Score · " + currentScore.ToString();
     }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        ispaused = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("NJ_gamescene");
+    }
+
+    public void LoadIslands()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Islands");
+    }
+    
 
     
 }
