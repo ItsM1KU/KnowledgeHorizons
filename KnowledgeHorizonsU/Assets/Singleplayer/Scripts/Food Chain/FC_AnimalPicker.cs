8b6fc833d44778e834b15ea59232f3e8bfd078ac
Sanjay <90610889+ItsM1KU@users.noreply.github.com>
2025-02-10 22:58:53 +0530

    Added more Rounds (need to fix Bugs)

--- KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Food Chain/FC_AnimalPicker.cs
+++ KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Food Chain/FC_AnimalPicker.cs
@@ -12,6 +12,34 @@ public class FC_AnimalPicker : MonoBehaviour
         new int[] {1, 2, 3},
         new int[] {2, 3, 5}
     };
+    
+    public List<int[]> secondAnimalSet = new List<int[]>
+    {
+        new int[] {0, 1, 2},
+        new int[] {1, 2, 3},
+        new int[] {2, 3, 4}
+    };
+
+    public List<int[]> thirdAnimalSet = new List<int[]>
+    {
+        new int[] {0, 1, 2, 3},
+        new int[] {1, 2, 3, 4},
+        new int[] {2, 3, 4, 5}
+    };
+
+    public List<int[]> fourthAnimalSet = new List<int[]>
+    {
+        new int[] {0, 1, 2, 4},
+        new int[] {1, 2, 3, 2},
+        new int[] {2, 3, 4, 5}
+    };
+
+    public List<int[]> fifthAnimalSet = new List<int[]>
+    {
+        new int[] {0, 1, 2, 3, 4},
+        new int[] {1, 2, 3, 4, 1},
+        new int[] {2, 3, 4, 5, 1}
+    };
 
     private void Awake()
     {
@@ -25,6 +53,34 @@ public class FC_AnimalPicker : MonoBehaviour
     {
         int randomIndex = Random.Range(0, firstAnimalSet.Count);
         int[] selectedSet = firstAnimalSet[randomIndex];
+        return selectedSet;
+    }
+
+    public int[] PickSecondSet()
+    {
+        int randomIndex = Random.Range(0, secondAnimalSet.Count);
+        int[] selectedSet = secondAnimalSet[randomIndex];
+        return selectedSet;
+    }
+
+    public int[] PickThirdSet()
+    {
+        int randomIndex = Random.Range(0, thirdAnimalSet.Count);
+        int[] selectedSet = thirdAnimalSet[randomIndex];
+        return selectedSet;
+    }
+
+    public int[] PickFourthSet()
+    {
+        int randomIndex = Random.Range(0, fourthAnimalSet.Count);
+        int[] selectedSet = fourthAnimalSet[randomIndex];
+        return selectedSet;
+    }
+
+    public int[] PickFifthSet()
+    {
+        int randomIndex = Random.Range(0, fifthAnimalSet.Count);
+        int[] selectedSet = fifthAnimalSet[randomIndex];
         return selectedSet;
     }
 }
--- KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Food Chain/FC_GameManager.cs
+++ KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Food Chain/FC_GameManager.cs
@@ -9,6 +9,10 @@ public class FC_GameManager : MonoBehaviour
     public static FC_GameManager instance;
 
     [SerializeField] GameObject[] slotsA;
+    [SerializeField] GameObject[] slotsB;
+    [SerializeField] GameObject[] slotsC;
+    [SerializeField] GameObject[] slotsD;
+    [SerializeField] GameObject[] slotsE;
 
     [SerializeField] GameObject[] animalPrefabs;
 
@@ -22,7 +26,14 @@ public class FC_GameManager : MonoBehaviour
     int currentPlace;
     //private bool canSubmit = false;
 
-    private List<string> correctChain = new List<string> { "Plant", "Ant", "Lion" };
+    [SerializeField] public List<GameObject[]> slotsPerRound = new List<GameObject[]>();
+
+
+    private int currentRound = 0;
+
+    
+    private List<GameObject> spawnedAnimals = new List<GameObject>();
+
     private void Awake()
     {
         if (instance == null)
@@ -30,7 +41,13 @@ public class FC_GameManager : MonoBehaviour
             instance = this;
         }
 
+        currentRound = 0;
 
+        slotsPerRound.Add(slotsA);
+        slotsPerRound.Add(slotsB);
+        slotsPerRound.Add(slotsC);
+        slotsPerRound.Add(slotsD);
+        slotsPerRound.Add(slotsE);
     }
 
     private void Start()
@@ -39,8 +56,39 @@ public class FC_GameManager : MonoBehaviour
         //ActivateSlots();
     }
 
+    private void PickRound()
+    {
+        switch (currentRound)
+        {
+            case 0:
+                SpawnFirstSet();
+                break;
+            case 1:
+                StartCoroutine(SecondRound());
+                break;
+            case 2:
+                StartCoroutine(ThirdRound());
+                break;
+            case 3:
+                StartCoroutine(FourthRound());
+                break;
+            case 4:
+                StartCoroutine(FifthRound());
+                break;
+            default:
+                Debug.Log("Game Over");
+                break;
+        }
+    }
     private void SpawnFirstSet()
     {
+        ActivateSlots(slotsA);
+        DeactivateSlots(slotsB);
+        DeactivateSlots(slotsC);
+        DeactivateSlots(slotsD);
+        DeactivateSlots(slotsE);
+
+
         AnswerSet = FC_AnimalPicker.instance.PickFirstSet();
         int[] selectedset = AnswerSet;
 
@@ -53,15 +101,144 @@ public class FC_GameManager : MonoBehaviour
             {
                 GameObject go = Instantiate(animalPrefabs[animalIndex], _spawnPoints[spawnIndices[i]].position, Quaternion.identity);
                 go.gameObject.transform.SetParent(Canvas.transform);
+                spawnedAnimals.Add(go);
             }
         }
     }
 
+    private void SpawnSecondSet()
+    {
+        DeactivateSlots(slotsA);
+        ActivateSlots(slotsB);
+        DeactivateSlots(slotsC);
+        DeactivateSlots(slotsD);
+        DeactivateSlots(slotsE);
+
+        DestroyAnimals(spawnedAnimals);
+
+        AnswerSet = FC_AnimalPicker.instance.PickSecondSet();
+        int[] selectedset = AnswerSet;
+
+        List<int> spawnIndices = new List<int> { 0, 1, 2};
+        shuffleList(spawnIndices);
+        for (int i = 0; i < selectedset.Length; i++)
+        {
+            int animalIndex = selectedset[i];
+            if (animalIndex >= 0 && animalIndex < animalPrefabs.Length)
+            {
+                GameObject go = Instantiate(animalPrefabs[animalIndex], _spawnPoints[spawnIndices[i]].position, Quaternion.identity);
+                go.gameObject.transform.SetParent(Canvas.transform);
+                spawnedAnimals.Add(go);
+            }
+        }
+    }
+
+    private void SpawnThirdSet()
+    {
+        DeactivateSlots(slotsA);
+        ActivateSlots(slotsC);
+        DeactivateSlots(slotsB);
+        DeactivateSlots(slotsD);
+        DeactivateSlots(slotsE);
+
+        DestroyAnimals(spawnedAnimals);
+
+        AnswerSet = FC_AnimalPicker.instance.PickThirdSet();
+        int[] selectedset = AnswerSet;
+
+        List<int> spawnIndices = new List<int> { 0, 1, 2, 3};
+        shuffleList(spawnIndices);
+        for (int i = 0; i < selectedset.Length; i++)
+        {
+            int animalIndex = selectedset[i];
+            if (animalIndex >= 0 && animalIndex < animalPrefabs.Length)
+            {
+                GameObject go = Instantiate(animalPrefabs[animalIndex], _spawnPoints[spawnIndices[i]].position, Quaternion.identity);
+                go.gameObject.transform.SetParent(Canvas.transform);
+                spawnedAnimals.Add(go);
+            }
+        }
+    }
+
+    private void SpawnFourthSet()
+    {
+        DeactivateSlots(slotsA);
+        ActivateSlots(slotsD);
+        DeactivateSlots(slotsC);
+        DeactivateSlots(slotsB);
+        DeactivateSlots(slotsE);
+
+        DestroyAnimals(spawnedAnimals);
+
+        AnswerSet = FC_AnimalPicker.instance.PickFourthSet();
+        int[] selectedset = AnswerSet;
+
+        List<int> spawnIndices = new List<int> { 0, 1, 2, 3};
+        shuffleList(spawnIndices);
+        for (int i = 0; i < selectedset.Length; i++)
+        {
+            int animalIndex = selectedset[i];
+            if (animalIndex >= 0 && animalIndex < animalPrefabs.Length)
+            {
+                GameObject go = Instantiate(animalPrefabs[animalIndex], _spawnPoints[spawnIndices[i]].position, Quaternion.identity);
+                go.gameObject.transform.SetParent(Canvas.transform);
+                spawnedAnimals.Add(go);
+            }
+        }
+    }
+
+    private void SpawnFifthSet()
+    {
+        DeactivateSlots(slotsA);
+        ActivateSlots(slotsB);
+        DeactivateSlots(slotsC);
+        DeactivateSlots(slotsD);
+        DeactivateSlots(slotsE);
+
+        DestroyAnimals(spawnedAnimals);
+
+        AnswerSet = FC_AnimalPicker.instance.PickFifthSet();
+        int[] selectedset = AnswerSet;
+
+        List<int> spawnIndices = new List<int> { 0, 1, 2, 3, 4};
+        shuffleList(spawnIndices);
+        for (int i = 0; i < selectedset.Length; i++)
+        {
+            int animalIndex = selectedset[i];
+            if (animalIndex >= 0 && animalIndex < animalPrefabs.Length)
+            {
+                GameObject go = Instantiate(animalPrefabs[animalIndex], _spawnPoints[spawnIndices[i]].position, Quaternion.identity);
+                go.gameObject.transform.SetParent(Canvas.transform);
+                spawnedAnimals.Add(go);
+            }
+        }
+    }
+
+
+
     public void submit()
     {
 
-        if (slotsA[0].GetComponent<FC_SlotHolder>().isoccupied && slotsA[1].GetComponent<FC_SlotHolder>().isoccupied && slotsA[2].GetComponent<FC_SlotHolder>().isoccupied)
+        if(currentRound > 5)
         {
+            Debug.Log("Game Over");
+        }
+
+        GameObject[] currentSlots = slotsPerRound[currentRound];
+
+        bool areOccupied = true;
+        foreach (GameObject slot in currentSlots)
+        {
+            if (!slot.GetComponent<FC_SlotHolder>().isoccupied)
+            {
+                areOccupied = false;
+                break;
+            }
+        }
+
+        if (areOccupied)
+        {
+            currentPlace = 0;
             for (int i = 0; i < AnswerSet.Length; i++)
             {
                 if (slotsA[i].transform.GetChild(0).GetComponent<FC_AnimalInfo>().AnimalID == AnswerSet[i])
@@ -77,13 +254,14 @@ public class FC_GameManager : MonoBehaviour
             if (currentPlace >= AnswerSet.Length)
             {
                 Debug.Log("You have completed the food chain");
-                currentPlace = 0;
+                currentRound++;
+                PickRound();
 
             }
             else
             {
                 Debug.Log("You have " + currentPlace + " Correct");
-                currentPlace = 0;
+                
             }
         }
         else
@@ -91,6 +269,31 @@ public class FC_GameManager : MonoBehaviour
             Debug.Log("CANT SUBMIT");
         }
     }
+
+    public IEnumerator SecondRound()
+    {
+        yield return new WaitForSeconds(1);
+        SpawnSecondSet();
+    }
+
+    public IEnumerator ThirdRound()
+    {
+        yield return new WaitForSeconds(1);
+        SpawnThirdSet();
+    }
+
+    public IEnumerator FourthRound()
+    {
+        yield return new WaitForSeconds(1);
+        SpawnFourthSet();
+    }
+
+    public IEnumerator FifthRound()
+    {
+        yield return new WaitForSeconds(1);
+        SpawnFifthSet();
+    }
+
 
     private void shuffleList(List<int> list)
     {
@@ -108,6 +311,22 @@ public class FC_GameManager : MonoBehaviour
         for (int i = 0; i < slots.Length; i++)
         {
             slots[i].SetActive(true);
+        }
+    }
+
+    private void DeactivateSlots(GameObject[] slots)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i].SetActive(false);
+        }
+    }
+
+    private void DestroyAnimals(List<GameObject> animals)
+    {
+        foreach (GameObject animal in animals)
+        {
+            Destroy(animal);
         }
     }
 
