81f908acc860b207aedcfb15aca26a04ce6c2230
Sanjay <90610889+ItsM1KU@users.noreply.github.com>
2025-02-26 12:49:14 +0530

    Created a Dialog Box

---
+++ KnowledgeHorizonsU/Assets/Singleplayer/Scripts/Time Vault/TV_DialogManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TV_DialogManager : MonoBehaviour
+{
+    public static TV_DialogManager Instance;
+
+    [SerializeField] List<string> dialogA;
+
+    [SerializeField] GameObject dialogBox;
+    [SerializeField] TextMeshProUGUI dialogText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartDialog(dialogA));
+    }
+
+
+    public IEnumerator StartDialog(List<string> dialog)
+    {
+        dialogText.text = "";
+        dialogBox.SetActive(true);
+
+        for (int i = 0; i < dialog.Count; i++)
+        {
+            dialogText.text = "";
+            yield return typeDialog(dialog[i]);
+            yield return new WaitForSeconds(1f);
+        }
+        yield return new WaitForSeconds(5f);
+        dialogBox.SetActive(false);
+
+    }
+
+    public IEnumerator typeDialog(string sentence)
+    {
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1/30f);
+        }
+    }
+}
